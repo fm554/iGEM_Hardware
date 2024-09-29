220a3cfdc553cc4774db28949012222906ba504e
fm554 <98335706+fm554@users.noreply.github.com>
2024-09-29 11:19:56 +0100

    linear_stage_monitor

--- Gus_ArduinoTests/linear_stage/linear_stage/linear_stage.ino
+++ Gus_ArduinoTests/linear_stage/linear_stage/linear_stage.ino
@@ -1,14 +1,14 @@
 #include <Stepper.h>
 
 // Define number of steps per revolution:
-const int stepsPerRevolution = 200;
+const int stepsPerRevolution = 20;
 
 // Initialize the stepper library on pins 8 through 11:
 Stepper myStepper = Stepper(stepsPerRevolution, 8, 9, 10, 11);
 
 void setup() {
   // Set the motor speed (RPMs):
-  myStepper.setSpeed(50);
+  myStepper.setSpeed(400);
 }
 
 void loop() {
